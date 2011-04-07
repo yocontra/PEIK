6541721edf2752e3bfab9be84e0671567cdb3691
FullmetalCracker <FullmetalCracker@gmail.com>
2011-04-07 00:43:24 -0700

    Optimized GetKey code

--- PEIK/Util/Crypter/CrypterMethods.cs
+++ PEIK/Util/Crypter/CrypterMethods.cs
@@ -9,22 +9,22 @@ namespace PEIK.Util.Crypter
 {
     internal class CrypterMethods
     {
-        public static string GetKey(int length){
-            Random ra = new Random();
-            StringBuilder s = new StringBuilder();
-            char[] b = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".ToCharArray();
-            for (int i = 1; i <= length; i++)
-            {
-                s.Append(b[ra.Next(0, b.Length)]);
-            }
-            return s.ToString();
+        static Random random = new Random();
+        public static string GetKey(int length)
+        {
+            const string str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            StringBuilder sb = new StringBuilder();
+
+            while ((length--) > 0)
+                sb.Append(str[(int)(random.NextDouble() * str.Length)]);
+
+            return sb.ToString();
         }
 
         public static byte[] Encrypt(byte[] message, string password)
         {
             byte[] passarr = Encoding.Default.GetBytes(password);
-            Random ra = new Random();
-            int rand = Convert.ToInt32(ra.Next(9001));
+            int rand = Convert.ToInt32(random.Next(9001));
             byte[] outarr = new byte[message.Length + 1];
             int u = 0;
             for (int i = 0; i <= message.Length - 1; i++)
